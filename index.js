3c3e4461c8bfb93a284d6116531355994018fbf4
gunturkh <guntur.kh@gmail.com>
2018-07-26 15:46:19 +0700

    Move result-field inside container

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
 $(function () {
     const addressForm = $("#address-form")
-    const resultForm = $("result-form")
+    const resultField = $("#result-field")
+
+    let contact = []
+
+
+    
 
 
     const addressDOM = (event) => {
         event.preventDefault()
 
-        const newAddress = {
-            firstName: $("#first-name").val(),
-            lastName: $("#last-name").val(),
-            phone: $("#phone").val(),
-            email: $("#email").val(),
-            city: $("#city").val()
+        let newAddress = {
+            "firstName": $("#first-name").val(),
+            "lastName": $("#last-name").val(),
+            "phone": $("#phone").val(),
+            "email": $("#email").val(),
+            "city": $("#city").val()
         }
 
-        console.log(newAddress);
+        const firstName = $("#first-name-result")
+        const lastName = $("#last-name-result")
+        const phone = $("#phone-result")
+        const email = $("#email-result")
+        const city = $("#city-result")
+
+        contact.push(newAddress)
+
+        const divChild = document.createElement('div')
+        divChild.innerHTML = `${newAddress.firstName}`
+        $('#result-field').append(`<div>First Name : ${newAddress.firstName}<br>
+        Last Name : ${newAddress.lastName}<br>
+        Phone : ${newAddress.phone}<br>
+        Email : ${newAddress.email}<br>
+        City : ${newAddress.city}<br>
+        </div>
+        `)
         
 
 
-        const result = $("#result-output")
 
-        result.text(newAddress[0])
+        firstName.text(`First Name : ${newAddress.firstName}`)
+        lastName.text(`Last Name : ${newAddress.lastName}`)
+        phone.text(`Phone : ${newAddress.phone}`)
+        email.text(`Email : ${newAddress.email}`)
+        city.text(`City : ${newAddress.city}`)
 
 
+
+        console.log(`First Name : ${newAddress.firstName}
+        Last Name : ${newAddress.lastName}
+        Phone : ${newAddress.phone}
+        Email : ${newAddress.email}
+        City : ${newAddress.city}`)
+
+        
 
     }
 
