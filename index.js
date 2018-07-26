7a8eecf1fad182a8a79d5677d944330d8280ea2e
gunturkh <guntur.kh@gmail.com>
2018-07-26 12:22:32 +0700

    Add result form

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 $(function () {
     const addressForm = $("#address-form")
-    
+    const resultForm = $("result-form")
+
+
     const addressDOM = (event) => {
         event.preventDefault()
-        const firstName = $("#first-name").val()
-        const lastName = $("#last-name").val()
-        const phone = $("#phone").val()
-        const email = $("#email").val()
-        const city = $("#city").val()
+
+        const newAddress = {
+            firstName: $("#first-name").val(),
+            lastName: $("#last-name").val(),
+            phone: $("#phone").val(),
+            email: $("#email").val(),
+            city: $("#city").val()
+        }
+
+        console.log(newAddress);
+        
+
+
+        const result = $("#result-output")
+
+        result.text(newAddress[0])
+
+
 
     }
 
@@ -15,6 +30,6 @@ $(function () {
 
     // Event Listener
     addressForm.on("submit", addressDOM)
-    
+
 })
 
